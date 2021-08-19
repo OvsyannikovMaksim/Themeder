7c709e95a87e3351704ad26500a1039fa4771bff
Ovsyannikov_M <maksim.ov@mail.ru>
2021-08-19 15:38:54 +0300

    add main screen fragment

--- app/src/main/java/com/example/myapplication5/MainActivity.kt
+++ app/src/main/java/com/example/myapplication5/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.example.myapplication5.databinding.ActivityMainBinding
+import androidx.fragment.app.FragmentTransaction
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,21 +13,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.toolbar.setNavigationOnClickListener{
             binding.mainActivity.openDrawer(GravityCompat.START)
         }
+
         binding.navigationView.setNavigationItemSelectedListener { menuItem->
             val itemId = menuItem.itemId
             when (itemId) {
-                R.id.main_screen_fragment ->  Log.d("Mainactivity","open main screen fragment")
+                R.id.main_screen_menu_item ->  Log.d("MainActivity","open main screen fragment")
                 //TODO: Добавить всем пунктам меню id и сделать переходы по фрагментам здесь
             }
 
             binding.mainActivity.closeDrawer(GravityCompat.START)
             true
+        }
+
+
+        if (savedInstanceState == null) {
+            fragmentTransaction
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .add(R.id.container, MainScreenFragment())
+                .commit()
         }
     }
 }
---
+++ app/src/main/java/com/example/myapplication5/MainScreenFragment.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication5
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.example.myapplication5.databinding.MainScreenFragmentBinding
+import com.yuyakaido.android.cardstackview.*
+
+
+class MainScreenFragment: Fragment() {
+    private lateinit var binding: MainScreenFragmentBinding
+    private lateinit var manager: CardStackLayoutManager
+    private lateinit var cardStackView: CardStackView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding= MainScreenFragmentBinding.inflate(inflater)
+        manager = CardStackLayoutManager(context)
+        cardStackView=binding.cardStackView
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cardStackViewInitialize()
+    }
+
+
+    private fun cardStackViewInitialize(){
+        manager.setStackFrom(StackFrom.None)
+        manager.setSwipeThreshold(0.3f)
+        manager.setMaxDegree(20.0f)
+        manager.setDirections(Direction.HORIZONTAL)
+        manager.setCanScrollHorizontal(true)
+        manager.setCanScrollVertical(false)
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+        manager.setOverlayInterpolator(LinearInterpolator())
+        cardStackView.layoutManager = manager
+        cardStackView.itemAnimator = DefaultItemAnimator()
+    }
+}
