9986d1363b741f21c5a62a9fc1cd45596bc534bb
Ovsyannikov_M <maksim.ov@mail.ru>
2023-01-10 17:30:41 +0300

    create main screen layout

--- app/src/main/java/com/themeder/themeder/ui/PhotosFragment.kt
+++ app/src/main/java/com/themeder/themeder/ui/PhotosFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.themeder.themeder.databinding.FragmentPhotosBinding
+import com.yuyakaido.android.cardstackview.CardStackView
 
 class PhotosFragment : Fragment() {
 
     private lateinit var binding: FragmentPhotosBinding
+    private lateinit var cardStackView: CardStackView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +20,10 @@ class PhotosFragment : Fragment() {
     ): View {
         binding = FragmentPhotosBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cardStackView = binding.cardStackView
     }
 }
